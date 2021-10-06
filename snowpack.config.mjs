9a7354993a51779be3afee11349dfd0a26a6165d
Manav Misra <manavm@visionwebsoft.com>
2021-10-06 12:01:59 -0500

    :wrench: TMDB_TOKEN

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -6,12 +6,10 @@ export default {
   },
   mount: {
     // directory name: 'build directory'
-    public: '/',
-    src: '/dist'
+    public: "/",
+    src: "/dist",
   },
-  plugins: 
-    ['@snowpack/plugin-react-refresh', '@snowpack/plugin-postcss',]
-  ,
+  plugins: ["@snowpack/plugin-react-refresh", "@snowpack/plugin-postcss"],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
@@ -24,9 +22,15 @@ export default {
     /* ... */
   },
   devOptions: {
-    tailwindConfig: './tailwind.config.js',
+    tailwindConfig: "./tailwind.config.js",
   },
   buildOptions: {
     /* ... */
+  },
+  env: {
+    // Adds to import.meta.env
+    TMDB_TOKEN:
+      // TODO: ❗ Replace this with your own!
+      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMzRiNjY2MjcwZDY2MDQyZDE0NTkxZmUxZGNiNzJiYiIsInN1YiI6IjVhZWM2M2IyMGUwYTI2NmUyNjAwNWRmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bnRmupVnSCiiXDZhF2lHZi4g8PsJCECTbsfPnz_iMIY",
   },
 };
